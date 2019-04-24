e69bae7e530a912f6387d94eb6f434706e09b403
Russell Koons <rkoons@gmail.com>
2019-04-24 10:08:32 -0400

    updated js

--- index.js
+++ index.js
@@ -6,40 +6,33 @@ $(function() {
     const plantry = $('#project1').offset().top - hgt;
     const training = $('#project2').offset().top - hgt;
     const genre = $('#project3').offset().top - hgt;
+    const portfolio = $('#project4').offset().top - hgt;
 
-    if ($(this).scrollTop() > plantry) {
-      $('#container').addClass('changepurple');
-      $('#hambutton').addClass('changepurple');
-      $('#projects').addClass('changepurple');
-    } 
-    if ($(this).scrollTop() > training && $(this).scrollTop() < genre) {
-      $('#container').removeClass('changepurple');
-      $('#hambutton').removeClass('changepurple');
-      $('#container').addClass('changegrey');
-      $('#hambutton').addClass('changegrey');
-    }
-    if ($(this).scrollTop() > genre) {
-      $('#container').removeClass('changegrey');
-      $('#hambutton').removeClass('changegrey');
-      $('#container').addClass('changewood');
-      $('#hambutton').addClass('changewood');
-    }
     if ($(this).scrollTop() < plantry) {
       $('#container').removeClass('changepurple');
       $('#hambutton').removeClass('changepurple');
-      $('#projects').removeClass('changepurple');
     }
     if ($(this).scrollTop() > plantry && $(this).scrollTop() < training) {
       $('#container').removeClass('changegrey');
       $('#hambutton').removeClass('changegrey');
       $('#container').addClass('changepurple');
       $('#hambutton').addClass('changepurple');
-    }
+    } 
     if ($(this).scrollTop() > training && $(this).scrollTop() < genre) {
-      $('#container').removeClass('changewood');
-      $('#hambutton').removeClass('changewood');
+      $('#container').removeClass('changepurple').removeClass('changewood');
+      $('#hambutton').removeClass('changepurple').removeClass('changewood');
       $('#container').addClass('changegrey');
       $('#hambutton').addClass('changegrey');
+    }
+    if ($(this).scrollTop() > genre && $(this).scrollTop() < portfolio) {
+      $('#container').removeClass('changegrey');
+      $('#hambutton').removeClass('changegrey');
+      $('#container').addClass('changewood');
+      $('#hambutton').addClass('changewood');
+    }
+    if ($(this).scrollTop() > portfolio) {
+      $('#container').removeClass('changewood');
+      $('#hambutton').removeClass('changewood');
     }
   });
 });
